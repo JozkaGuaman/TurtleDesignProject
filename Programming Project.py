9ad7f9ac6e429e0286c9897a485f6cad88dd4104
jozka <33328079+JozkaGuaman@users.noreply.github.com>
2017-11-06 13:33:27 -0500

    Add files via upload

---
+++ Programming Project.py
@@ -0,0 +1,708 @@
+#creating the moon and stars
+import turtle
+turtle.colormode(255)
+turtle.bgcolor(0,0,0)
+bob = turtle.Turtle()
+bob.speed(15)
+bob.color("black")
+bob.width(5)
+
+#creating the moon
+bob.begin_fill()
+for times in range(1):
+
+    c = (170,170,170)
+    bob.color(c)#c is the variable
+    bob.circle(200)
+    bob.right(200)
+    bob.forward(5)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(55)
+bob.forward(200)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(7)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(5)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(7)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(7)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(7)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(7)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(1)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(70)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(90)
+bob.forward(160)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(6)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(12)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(12)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(12)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.penup()
+bob.right(12)
+bob.forward(3)
+bob.pendown()
+
+bob.color("gray")
+bob.circle(30)
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(20)
+    bob.right(135)
+bob.penup()
+bob.right(90)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(20)
+    bob.right(135)
+bob.penup()
+bob.right(90)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(20)
+    bob.right(135)
+bob.penup()
+bob.right(90)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(20)
+bob.forward(170)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(20)
+    bob.right(135)
+bob.penup()
+bob.right(20)
+bob.forward(170)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(20)
+bob.forward(170)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(179)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(3)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(50)
+    bob.right(135)
+bob.penup()
+bob.right(200)
+bob.forward(200)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(140)
+bob.forward(250)
+bob.pendown()
+
+bob.end_fill()
+
+
+
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(140)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+bob.begin_fill()
+bob.color("yellow")
+for times in range(8):
+    
+    bob.forward(60)
+    bob.right(135)
+bob.penup()
+bob.right(180)
+bob.forward(150)
+bob.pendown()
+
+bob.end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+turtle.done()
+
+    
+
+    
